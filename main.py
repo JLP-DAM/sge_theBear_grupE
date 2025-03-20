99fb51b78e7c2b0f465654e73278795e0bb58c09
Set-Catalan <setcatalanribolleda@gmail.com>
2025-03-20 12:24:42 +0100

    commit de classe

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ async def create_item(name: str):
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
-
 engine = create_engine(sqlite_url, echo=True)
 
 @app.put("/items/")
